adabf069be83119af9b6abf1149f8903e740d13c
Edmundo Paulino <manyymoore@gmail.com>
2021-11-02 17:41:11 +0000

    chore: 💡 Add info about Separation of Concerns

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 use std::process;
 
 use minigrep::Config;
+// Separation of Concerns for Binary Projects
+// This pattern is about separating concerns: main.rs handles running the program, and lib.rs handles all the logic of the task at hand.
+// Because you can’t test the main function directly, this structure lets you test all of your program’s logic by moving it into functions in lib.rs.
+// The only code that remains in main.rs will be small enough to verify its correctness by reading it. Let’s rework our program by following this process.
 
 fn main() {
     let args: Vec<String> = env::args().collect();
